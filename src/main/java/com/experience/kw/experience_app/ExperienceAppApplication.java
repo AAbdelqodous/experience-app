51e12956879cac82eb1a6ab9c893bc042e94978b
AAbdelqodous <ml.ahmed1187@gmail.com>
2024-06-17 18:51:16 +0300

    start app

---
+++ src/main/java/com/experience/kw/experience_app/ExperienceAppApplication.java
@@ -0,0 +1,13 @@
+package com.experience.kw.experience_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperienceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experience/kw/experience_app/ExperienceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experience.kw.experience_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperienceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
